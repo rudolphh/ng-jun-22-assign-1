13dd80b46bddbb845ad8c68a7fae3c0972292b14
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-06-16 22:43:29 -0700

    part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-practice';
+
+  input1 : string = '';
+  input2 : string = '';
+  input3 : string = '';
+
+  items : string[] = [];
+
+
+  @ViewChild('div') div !: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+
+  }
+
+  addElement() {
+    const ul: HTMLUListElement = this.renderer.createElement('ul');
+    ul.innerHTML = `<li>${this.input1}</li>`;
+    ul.innerHTML += `<li>${this.input2}</li>`;
+    ul.innerHTML += `<li>${this.input3}</li>`;
+    this.renderer.appendChild(this.div.nativeElement, ul)
+  }
+
+  onSubmit() {
+    this.items.push(this.input1);
+    this.items.push(this.input2);
+    this.items.push(this.input3);
+  }
+
+  remove(index: number) {
+    this.items.splice(index, 1);
+    console.log(this.items);
+  }
+
+
 }
